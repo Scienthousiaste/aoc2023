9f5a04b440b261120aa3ea4e56d9ebb3b410e7fc
Timothée Behra <timothee.behra@aworldforus.com>
2023-12-07 22:12:53 +0100

    day 07 part 2

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -41,11 +41,11 @@ defmodule AdventOfCode.Day07 do
   def input(test? \\ false) do
     if test? do
       """
-        32T3K 765
-        T55J5 684
-        KK677 28
-        KTJJT 220
-        JJJJJ 483
+      32T3K 765
+      T55J5 684
+      KK677 28
+      KTJJT 220
+      QQQJA 483
       """
     else
       AdventOfCode.Input.get!(7, 2023)
@@ -53,7 +53,7 @@ defmodule AdventOfCode.Day07 do
   end
 
   def parse do
-    input()
+    input(false)
     |> String.split("\n", trim: true)
     |> Enum.map(fn line ->
       [hand, bid] = String.split(line)
@@ -66,7 +66,7 @@ defmodule AdventOfCode.Day07 do
     |> Enum.sort(fn h1, h2 ->
       # The given function should compare two arguments, and return true if
       # the first argument precedes or is in the same place as the second one.
-      if is_hand_better?(h1, h2) do
+      if is_hand_better?(h1, h2, &category/1, &card_value1/1) do
         false
       else
         true
@@ -79,10 +79,9 @@ defmodule AdventOfCode.Day07 do
     end)
   end
 
-  def is_hand_better?(%{hand: h1}, %{hand: h2}) do
-    c1 = category(h1)
-    c2 = category(h2)
-
+  def is_hand_better?(%{hand: h1}, %{hand: h2}, category, card_value) do
+    c1 = category.(h1)
+    c2 = category.(h2)
 
     cond do
       c1 > c2 -> true
@@ -92,7 +91,7 @@ defmodule AdventOfCode.Day07 do
         |> Enum.reduce_while(false, fn {card1, card2}, acc ->
           cond do
             card1 == card2 -> {:cont, acc}
-            is_better?(card1, card2) -> {:halt, true}
+            is_better?(card1, card2, card_value) -> {:halt, true}
             true -> {:halt, false}
           end
         end)
@@ -121,10 +120,79 @@ defmodule AdventOfCode.Day07 do
     end
   end
 
-  def is_better?(card1, card2) do
-    card_value1(card1) >= card_value1(card2)
+  def category2(hand) do
+    res = Enum.reduce(hand, %{}, fn char, acc ->
+      case Map.get(acc, char) do
+        nil -> Map.put(acc, char, 1)
+        x -> Map.put(acc, char, x + 1)
+      end
+    end)
+
+    new_res = if res["J"] do
+      nb_jokers = res["J"]
+      # en premier, meilleur nombre + meilleurs carte
+      other_card_comb = res
+      |> Map.drop(["J"])
+      |> Map.to_list()
+      |> Enum.sort(fn {card1, nb1}, {card2, nb2} ->
+
+        # The given function should compare two arguments, and return true if
+        # the first argument precedes or is in the same place as the second one
+        cond do
+          nb1 > nb2 -> true
+          nb2 > nb1 -> false
+          true -> not is_better?(card1, card2, &card_value2/1)
+        end
+      end)
+
+
+      if other_card_comb == [] do
+        res
+      else
+        [{best_card, best_nb} | rest] = other_card_comb
+
+        sup = [{best_card, best_nb + nb_jokers} | rest]
+        |> Map.new()
+
+        sup
+      end
+    else
+      res
+    end
+
+    values = Map.values(new_res)
+    set_values = MapSet.new(values)
+
+    cond do
+      MapSet.equal?(set_values, MapSet.new([5])) -> 999
+      MapSet.equal?(set_values, MapSet.new([4, 1])) -> 100
+      MapSet.equal?(set_values, MapSet.new([2, 3])) -> 50
+      MapSet.equal?(set_values, MapSet.new([1, 3])) -> 20
+      Enum.count(values) == 3 and MapSet.equal?(set_values, MapSet.new([1, 2])) -> 5
+      Enum.count(values) == 4 and MapSet.equal?(set_values, MapSet.new([1, 2])) -> 3
+      true -> 1
+    end
+  end
+
+  def is_better?(card1, card2, card_value) do
+    card_value.(card1) >= card_value.(card2)
   end
 
   def part2(_args \\[]) do
+    res = parse()
+    |> Enum.sort(fn h1, h2 ->
+      if is_hand_better?(h1, h2, &category2/1, &card_value2/1) do
+        false
+      else
+        true
+      end
+    end)
+
+    res
+    |> Enum.map(&(&1.bid))
+    |> Enum.with_index()
+    |> Enum.reduce(0, fn {bid, idx}, acc ->
+      acc + (bid * (idx + 1))
+    end)
   end
 end
