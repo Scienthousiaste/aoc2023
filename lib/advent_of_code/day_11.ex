d4991b35b74bf5612c980e8395deaf0b41ab55eb
Timothée Behra <timothee.behra@aworldforus.com>
2023-12-11 21:32:30 +0100

    day 11 part 2

--- lib/advent_of_code/day_11.ex
+++ lib/advent_of_code/day_11.ex
@@ -1,12 +1,9 @@
 defmodule AdventOfCode.Day11 do
 
+  @universe_expansion 999_999
+
   def input(test? \\ false) do
     if test? do
-      # """
-      # ...
-      # .#.
-      # ...
-      # """
       """
       ...#......
       .......#..
@@ -90,9 +87,6 @@ defmodule AdventOfCode.Day11 do
 
       if (idx + 1< n_galaxies) do
         Enum.reduce((idx+1)..(n_galaxies - 1), sum_distances, fn n, cur_sum ->
-          if (Enum.at(galaxies, n) == nil) do
-            require IEx; IEx.pry
-          end
           cur_sum + distance_between(galaxy, Enum.at(galaxies, n))
         end)
       else
@@ -101,6 +95,60 @@ defmodule AdventOfCode.Day11 do
     end)
   end
 
+  def pseudo_expand_universe(lines) do
+    max_x = lines
+    |> List.first
+    |> Enum.count
+    |> Kernel.-(1)
+
+    x_warps = Enum.reduce(0..max_x, [], fn x, warps ->
+      if Enum.all?(lines, fn line -> Enum.at(line, x) == "." end) do
+        [x | warps]
+      else
+        warps
+      end
+    end)
+
+    y_warps = Enum.reduce(Enum.with_index(lines), [], fn {line, idx}, warps ->
+      if Enum.all?(line, fn c -> c == "." end) do
+        [idx | warps]
+      else
+        warps
+      end
+    end)
+    |> Enum.reverse
+
+    %{x_warps: x_warps, y_warps: y_warps}
+  end
+
+  def nb_warps(x2, x1, x_warps) do
+    Enum.count(x_warps, fn w ->
+
+      (w > x2 and w < x1) or (w > x1 and w < x2)
+    end)
+  end
+
+  def warped_distance({x1, y1}, {x2, y2}, %{x_warps: x_warps, y_warps: y_warps}) do
+    x_dif = abs(x2 - x1) + nb_warps(x2, x1, x_warps) * @universe_expansion
+    y_dif = abs(y2 - y1) + nb_warps(y2, y1, y_warps) * @universe_expansion
+    x_dif + y_dif
+  end
+
   def part2(_args \\ []) do
+    lines = parse()
+    warp_zones = pseudo_expand_universe(lines)
+    galaxies = find_galaxies(lines)
+
+    n_galaxies = Enum.count(galaxies)
+
+    Enum.reduce(Enum.with_index(galaxies), 0, fn {galaxy, idx}, sum_distances ->
+      if (idx + 1< n_galaxies) do
+        Enum.reduce((idx+1)..(n_galaxies - 1), sum_distances, fn n, cur_sum ->
+          cur_sum + warped_distance(galaxy, Enum.at(galaxies, n), warp_zones)
+        end)
+      else
+        sum_distances
+      end
+    end)
   end
 end
