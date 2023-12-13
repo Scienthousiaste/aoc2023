a58af9077baffa9493a0c78d79d0eeaf8c50c0cc
Timothée Behra <timothee.behra@aworldforus.com>
2023-12-13 22:07:58 +0100

    day 12 part 2, successful memoization

--- lib/advent_of_code/day_12.ex
+++ lib/advent_of_code/day_12.ex
@@ -3,11 +3,6 @@ defmodule AdventOfCode.Day12 do
     if test do
       """
       ???.### 1,1,3
-      .??..??...?##. 1,1,3
-      ?#?#?#?#?#?#?#? 1,3,1,6
-      ????.#...#... 4,1,1
-      ????.######..#####. 1,6,5
-      ?###???????? 3,2,1
       """
       # ????.######..#####. 1,6,5
 
@@ -29,7 +24,6 @@ defmodule AdventOfCode.Day12 do
     else
       # AdventOfCode.Input.get!(12, 2023)
       {:ok, contents} = File.read("lib/advent_of_code/day_12_input.txt")
-      require IEx; IEx.pry
       contents
     end
   end
@@ -37,8 +31,6 @@ defmodule AdventOfCode.Day12 do
   def parse_lines(lines) do
     Enum.reduce(lines, [], fn line, list ->
       [l, r] = String.split(line)
-      # {String.split(l, ".", trim: true), String.split(r, ",")}
-      # garder la string à droite c'est plus simple à traiter après...
       nums = String.split(r, ",") |> Enum.map(&String.to_integer(&1))
       [{l, nums} | list]
     end)
@@ -63,7 +55,6 @@ defmodule AdventOfCode.Day12 do
       String.length(input) < n_to_match -> {false, ""}
 
       true ->
-        # require IEx; IEx.pry
         {
           !String.contains?(String.slice(input, 0, n_to_match), ".") and
             String.at(input, n_to_match) != "#",
@@ -74,10 +65,8 @@ defmodule AdventOfCode.Day12 do
 
   def count_arrangements({s, []}, memo, n) do
     if String.contains?(s, "#") do
-      # IO.inspect("finished with #{n}, #{s} contains #")
       {n, memo}
     else
-      # IO.inspect("Ok, did it! finished with #{n + 1}, #{s}")
       {n + 1, memo}
     end
   end
@@ -86,40 +75,37 @@ defmodule AdventOfCode.Day12 do
     {n, memo}
   end
 
-  def count_arrangements({string, [to_match | tail] = matches} = params, memo, number_found) do
+  def count_arrangements({string, [to_match | tail]} = params, memo, init_number) do
     memo_key = key(params)
     memoized_value = Map.get(memo, memo_key)
 
-    # if memoized_value do
-    #   {memoized_value, memo}
-    # else
-      Enum.reduce_while(0..(String.length(string) - 1), {number_found, memo}, fn idx, {n, memo} ->
-        # IO.inspect("Reduce while on #{string}, index: #{idx}, n: #{n}")
+    if memoized_value do
+      {init_number + memoized_value, memo}
+    else
+      {final_n, final_memo} = Enum.reduce_while(0..(String.length(string) - 1), {init_number, memo}, fn idx, {n, memo} ->
 
         input = String.slice(string, idx, 1000)
 
-        {nr, mr} = case match(input, to_match) do
+        {res_number, res_memo} = case match(input, to_match) do
           {true, next_string} ->
-            # require IEx; IEx.pry
-            # IO.inspect("matched #{input} with #{to_match}, #{n}")
             count_arrangements({next_string, tail}, memo, n)
           _ ->
-            # require IEx; IEx.pry
-            # je ne memoize que là, et pas encore sûr que c'est ok
-            {n, Map.put(memo, key({input, matches}), 0)}
+            {n, memo}
+
+            # {n, Map.put(memo, key({input, to_match}), 0)}
         end
 
         if String.starts_with?(input, "#") do
-          # IO.inspect("halt with r #{nr}, input #{input}, to_match #{to_match}")
-          {:halt, {nr, mr}}
+          # put in memo res_number - n ? res_numbe r - number_found ?
+          {:halt, {res_number, res_memo}}
         else
-          # IO.inspect("cont with r #{nr}")
-          {:cont, {nr, mr}}
+          {:cont, {res_number, res_memo}}
         end
       end)
 
-
-    # end
+      # require IEx; IEx.pry
+      {final_n, Map.put(final_memo, memo_key, (final_n - init_number) )}
+    end
   end
 
   def parse() do
@@ -132,15 +118,20 @@ defmodule AdventOfCode.Day12 do
     {n, _memo} = parse()
     |> Enum.reduce({0, %{}}, fn line, {cur_n, cur_memo} ->
       count_arrangements(line, cur_memo, cur_n)
-
-      # {res, Map.put(memo, key(line), res)}
-
     end)
-
-    # require IEx; IEx.pry
     n
   end
 
+  def unfold({l, r}) do
+    {Enum.join([l, l, l, l, l], "?"), r ++ r ++ r ++ r ++ r}
+  end
+
   def part2(_args \\ []) do
+    {n, _memo} = parse()
+    |> Enum.map(fn line -> unfold(line) end)
+    |> Enum.reduce({0, %{}}, fn line, {cur_n, cur_memo} ->
+      count_arrangements(line, cur_memo, cur_n)
+    end)
+    n
   end
 end
